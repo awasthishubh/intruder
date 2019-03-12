0add5aae77d8111b4f5d70f97abf4707a9759801
Shubham Awasthi <aw.shubh@gmail.com>
2019-03-12 17:35:28 +0530

    Updates

---
+++ precache-manifest.ae9604e77a7d4d8038195dc5c95e882e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d0d7964af6467216f7c1",
+    "url": "/static/js/main.c02437e4.chunk.js"
+  },
+  {
+    "revision": "1275e0e3083e60253db4",
+    "url": "/static/js/2.1417bfff.chunk.js"
+  },
+  {
+    "revision": "d0d7964af6467216f7c1",
+    "url": "/static/css/main.3b6f3dc7.chunk.css"
+  },
+  {
+    "revision": "728657b1dcf7d6764fb51034d4a0a794",
+    "url": "/index.html"
+  }
+];
